5ee177f1f22a58a180173cebd52c0bd4432e40ea
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-11-25 20:39:40 +0530

    refractor: unused imports

--- src/commands/witeboard.rs
+++ src/commands/witeboard.rs
@@ -1,4 +1,4 @@
-use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use uuid::Uuid;
@@ -7,7 +7,7 @@ use uuid::Uuid;
 #[description = "Whiteboard"]
 #[description = "gives link to whiteboard"]
 #[description = "Usage: pls.give board"]
-pub async fn board(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+pub async fn board(ctx: &Context, msg: &Message) -> CommandResult {
     let say_content = format!("https://witeboard.com/{}.", Uuid::new_v4());
     msg.reply(&ctx.http, say_content).await?;
     Ok(())
