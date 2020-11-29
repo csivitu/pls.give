cf1335f97ebd793bd3d07bddda76e0226fcc7a80
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-11-29 23:55:59 +0530

    [WIP] Isahc port

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -2,16 +2,29 @@ use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
+use isahc::prelude::*;
+use std::time::Duration;
+
+use url::Url;
+
 use tokio::task;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 async fn get_link(link: String) -> Result<String> {
-    let response = reqwest::Client::new()
-        .post("http://gg.gg/create")
-        .form(&[("long_url", link)])
-        .send()
-        .await?;
-    Ok(response.text().await?)
+
+    let url_parse = Url::parse(&link);
+    
+    if url_parse.is_err() {
+        return Ok(String::from("Invalid URL format"))
+    }
+
+    let mut response = Request::post("http://gg.gg/create")
+        .timeout(Duration::from_secs(5))
+        .body(format!(r#"long_url={}"#, link))?
+        .send()?;
+    let resp = response.text()?;
+    println!("Response is :|{}|", resp);
+    Ok(resp)
 }
 
 #[command]
